8ba08a84cfb95115a7f71dc9d08c3c6300f1b728
Administrator <123@123.com>
2018-05-04 10:36:55 +0800

    Initial commit

---
+++ app/src/main/res/raw/startup.bat
@@ -0,0 +1,5 @@
+start /d "C:\Program Files (x86)\Google\Chrome\Application\" chrome.exe
+start /d D:\cmder\ Cmder.exe
+start /d "C:\Program Files (x86)\NetSarang\Xshell 5\" Xshell.exe
+start /d "D:\Android\Android Studio\bin\" studio64.exe
+start /d D:\directoryopus12\DOPUS\ dopus.exe
